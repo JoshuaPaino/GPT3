41096abf40247da0a4a08ff4de0eba0f35364e90
JoshuaPaino <joshua.paino@gmail.com>
2022-11-25 17:37:34 -0600

    add: App Styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 //components
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3, WHatGPT3 } from "./containers";
 import { Cta, Brand, Navbar } from "./components";
+import './App.css';
 
 const App = () => {
     return (
